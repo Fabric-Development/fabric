68dfd56ae269f7575448c0255cea3983f0316a6a
The Darsh <129121284+its-darsh@users.noreply.github.com>
2024-01-24 08:01:34 +0200

    update: fabricator now have more signals, removing unused variables

--- fabric/utils/fabricator/fabricator.py
+++ fabric/utils/fabricator/fabricator.py
@@ -14,7 +14,9 @@ class PollingMode(ValueEnum):
 class Fabricate(Service):
     __gsignals__ = SignalContainer(
         Signal("changed", "run-first", None, (object,)),
-        Signal("polling", "run-first", None, (object,)),
+        Signal("polling", "run-first", None, (object, object, int, object)),
+        Signal("polling-function", "run-first", None, (object,)),
+        Signal("polling-shell-command", "run-first", None, (object,)),
         Signal("polling-done", "run-first", None, (object,)),
     )
 
@@ -22,7 +24,6 @@ class Fabricate(Service):
         self,
         value: object | None = None,
         poll_from: Callable | str | None = False,
-        poll_if: Callable | str | None = None,
         interval: int | None = None,
         stop_when: object | None = None,
         initial_poll: bool = True,
@@ -34,7 +35,6 @@ class Fabricate(Service):
         )
         self._value = value
         self._poll_from = poll_from
-        self._poll_if = poll_if
         self._interval = interval
         self._stop_when = stop_when
         self._initial_poll = initial_poll
@@ -60,8 +60,10 @@ class Fabricate(Service):
         self, mode: PollingMode, callable: Callable, interval: int | None = None, *args
     ):
         if mode == PollingMode.NONE:
+            self.emit("polling-done", callable)
             return logger.info("[Fabricator] Polling mode is set to None, skipping...")
         elif mode == PollingMode.FUNCTION:
+            self.emit("polling-function", callable)
             return (
                 self.invoke_function_with_interval(callable, interval, *args),
                 self.do_invoke_function(callable, *args)
@@ -69,6 +71,7 @@ class Fabricate(Service):
                 else None,
             )
         elif mode == PollingMode.SHELL_COMMAND:
+            self.emit("polling-shell-command", callable)
             return (
                 self.invoke_shell_with_interval(callable, interval),
                 print(self.do_invoke_shell(callable))
@@ -84,6 +87,7 @@ class Fabricate(Service):
         )
 
     def do_invoke_shell(self, cmd: str):
+        self.emit("polling", self._polling_mode, cmd, self._interval, ())
         if self._poll is True:
             value = exec_shell_command(cmd)
             if value == self._stop_when:
@@ -98,6 +102,7 @@ class Fabricate(Service):
         return invoke_repeater(interval, self.do_invoke_function, callable, *args)
 
     def do_invoke_function(self, callable: Callable, *args):
+        self.emit("polling", self._polling_mode, callable, self._interval, args)
         if self._poll is True:
             value = callable(*args)
             if value == self._stop_when:

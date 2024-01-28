d4567e3a18ebd67fb308ab88da6286b6d48510a4
The Darsh <129121284+its-darsh@users.noreply.github.com>
2024-01-28 20:11:03 +0200

    refactor: the entire service file, bug fixes, new features

--- fabric/service.py
+++ fabric/service.py
@@ -4,63 +4,6 @@ from gi.repository import GObject
 from gi._propertyhelper import Property
 
 
-@dataclass
-class Signal:
-    name: str
-    flags: Literal[
-        "run-last",
-        "run-first",
-        "run-cleanup",
-        "no-recurse",
-        "detailed",
-        "action",
-        "no-hooks",
-        "must-collect",
-        "deprecated",
-        "accumulator-first-run",
-    ] | GObject.SignalFlags = "run-first"
-    rtype: Type | object = None
-    args: tuple[Type | object] = ()
-
-
-class SignalContainer(dict):
-    """
-    a class that conatins signals to get added later to the __gsignals__ field,
-    it provides you a high level of functionality to work with since
-    it converts passed `Signal` objects into a `dict` object so
-    it can probably be used with __gsignals__
-    """
-
-    def __init__(self, *args: list[Signal]):
-        _signals = {}
-        for sig in args:
-            sig: Signal
-            if isinstance(sig, Signal) is True:
-                _signals[sig.name] = (
-                    sig.flags
-                    if isinstance(sig.flags, GObject.SignalFlags) is True
-                    else {
-                        "run-last": GObject.SignalFlags.RUN_LAST,
-                        "run-first": GObject.SignalFlags.RUN_FIRST,
-                        "run-cleanup": GObject.SignalFlags.RUN_CLEANUP,
-                        "no-recurse": GObject.SignalFlags.NO_RECURSE,
-                        "detailed": GObject.SignalFlags.DETAILED,
-                        "action": GObject.SignalFlags.ACTION,
-                        "no-hooks": GObject.SignalFlags.NO_HOOKS,
-                        "must-collect": GObject.SignalFlags.MUST_COLLECT,
-                        "deprecated": GObject.SignalFlags.DEPRECATED,
-                        "accumulator-first-run": GObject.SignalFlags.ACCUMULATOR_FIRST_RUN,
-                    }.get(
-                        sig.flags, GObject.SignalFlags.RUN_FIRST
-                    ),  # no inline switch? (FIXME: use getattr)
-                    sig.rtype,
-                    sig.args,
-                )
-            else:
-                continue
-        super().__init__(_signals)
-
-
 class Property(Property):
     """
     replace python's `property` built-in decorator with this decorator
@@ -143,10 +86,77 @@ class Property(Property):
         return super().setter(fset)
 
 
+@dataclass
+class Signal:
+    name: str
+    flags: Literal[
+        "run-last",
+        "run-first",
+        "run-cleanup",
+        "no-recurse",
+        "detailed",
+        "action",
+        "no-hooks",
+        "must-collect",
+        "deprecated",
+        "accumulator-first-run",
+    ] | GObject.SignalFlags = "run-first"
+    rtype: Type | object = None
+    args: tuple[Type | object] = ()
+
+
+class SignalContainer(dict):
+    """
+    a class that conatins signals to get added later to the __gsignals__ field,
+    it provides you a high level of functionality to work with since
+    it converts passed `Signal` objects into a `dict` object so
+    it can probably be used with __gsignals__
+    """
+
+    def __init__(self, *args: list[Signal]):
+        _signals = {}
+        for sig in args:
+            sig: Signal
+            if isinstance(sig, Signal) is True:
+                _signals[sig.name] = (
+                    sig.flags
+                    if isinstance(sig.flags, GObject.SignalFlags) is True
+                    else {
+                        "run-last": GObject.SignalFlags.RUN_LAST,
+                        "run-first": GObject.SignalFlags.RUN_FIRST,
+                        "run-cleanup": GObject.SignalFlags.RUN_CLEANUP,
+                        "no-recurse": GObject.SignalFlags.NO_RECURSE,
+                        "detailed": GObject.SignalFlags.DETAILED,
+                        "action": GObject.SignalFlags.ACTION,
+                        "no-hooks": GObject.SignalFlags.NO_HOOKS,
+                        "must-collect": GObject.SignalFlags.MUST_COLLECT,
+                        "deprecated": GObject.SignalFlags.DEPRECATED,
+                        "accumulator-first-run": GObject.SignalFlags.ACCUMULATOR_FIRST_RUN,
+                    }.get(
+                        sig.flags, GObject.SignalFlags.RUN_FIRST
+                    ),  # no inline switch? (FIXME: use getattr)
+                    sig.rtype,
+                    sig.args,
+                )
+            else:
+                continue
+        super().__init__(_signals)
+
+
 @dataclass(frozen=True)
-class SignalCallback:
+class SignalConnection:
+    """
+    dataclass that holds a reference to a signal and its callback, name and other info
+    """
+
+    id: int
     name: str
     callback: Callable
+    owner: GObject.Object
+
+    def disconnect(self):
+        """disconnects the signal"""
+        return self.owner.disconnect(self)
 
 
 class Service(GObject.Object):
@@ -154,47 +164,56 @@ class Service(GObject.Object):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._registered_callbacks: list[SignalCallback] = []
+        self._registered_signals: list[SignalConnection] = []
 
-    def disconnect(self, reference: SignalCallback | Callable | str) -> list[int]:
-        """disconnects a signal
-        by passing the `SignalCallback` to `reference` the callback function will gets disconnected.
+    def disconnect(
+        self, reference: SignalConnection | Callable | str | int
+    ) -> list[int]:
+        """
+        disconnects a signal
+        by passing the `SignalConnection` to `reference` the callback function will gets disconnected.
 
         passing a function or a callable makes the method search the props looking for a
         matching object and disconnect it
 
         if passed a string it will disconnect every callback uses the same signal string
 
-        :param reference: a `SignalCallback`, the callback function or a string contains the signal name
-        :type reference: SignalCallback | Callable | str
-        :raises HyprlandReferenceError: if no matches for the reference passed
+        :param reference: a `SignalConnection`, the callback function or a string contains the signal name
+        :type reference: SignalConnection | Callable | str | int
+        :raises ReferenceError: if no matches for the passed reference
         :return: a list of results of the disconnect function for everything have something to do with passed reference
         :rtype: list[int]
         """
         signal_objects = []
-        if isinstance(reference, SignalCallback):
-            if reference in self._registered_callbacks:
-                signal_objects.append(signal)
+        if isinstance(reference, SignalConnection):
+            if reference in self._registered_signals:
+                signal_objects.append(reference)
         elif isinstance(reference, Callable):  # reference is a function.
-            for signal in self._registered_callbacks:
-                if signal.callback == reference:
+            for signal in self._registered_signals:
+                if reference == signal.callback:
                     signal_objects.append(signal)
                     break
-        else:  # maybe its a string, if string so remove every object with same string.
-            for signal in self._registered_callbacks:
-                if signal.name == reference:
+        elif isinstance(
+            reference, str
+        ):  # maybe its a string, if string so remove every object with same string.
+            for signal in self._registered_signals:
+                if reference == signal.name:
                     signal_objects.append(signal)
-        if len(signal_objects) > 0:
-            [self._registered_callbacks.remove(x) for x in signal_objects]
-            return [self.disconnect_by_func(x.reference) for x in signal_objects]
-        else:
+                    break
+        elif isinstance(reference, int):  # the signal connection id
+            for signal in self._registered_signals:
+                if reference == signal.id:
+                    signal_objects.append(signal)
+                    break
+        if len(signal_objects) < 1:
             raise ReferenceError(
                 f"reference {reference} haven't been registered here before."
             )
+        for signal in signal_objects:
+            self._registered_signals.remove(signal)
+        return [self.disconnect_by_func(x.callback) for x in signal_objects]
 
-    def connect(
-        self, signal_spec: str, callback: Callable
-    ) -> tuple[object, SignalCallback]:
+    def connect(self, signal_spec: str, callback: Callable) -> SignalConnection:
         """
         connects you to a signal
         requires you passing the signal name as a string
@@ -204,9 +223,14 @@ class Service(GObject.Object):
         :type signal_spec: str
         :param callback: the callback function that will get called whenever the signal gets emitted, note that this should at least have one argument
         :type callback: Callable
-        :return: a tuple contains the returned data of the connect method and the SignalCallback object that makes you able to disconnect later
-        :rtype: tuple[object, SignalCallback]
+        :return: the `SignalConnection` object that contains the connection information
+        :rtype: SignalConnection
         """
-        sc = SignalCallback(name=signal_spec, callback=callback)
-        self._registered_callbacks.append(sc)
-        return super().connect(signal_spec, callback), sc
+        conn = SignalConnection(
+            id=super().connect(signal_spec, callback),
+            name=signal_spec,
+            callback=callback,
+            owner=self,
+        )
+        self._registered_signals.append(conn)
+        return conn

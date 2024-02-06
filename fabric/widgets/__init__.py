dd4272de0019fb5f0d3ed14f27d5d7c863cdff25
BoolmanO <ponos585858@gmail.com>
2024-02-06 15:19:09 +0600

    add all widgets to __init__.py in widgets folder.

---
+++ fabric/widgets/__init__.py
@@ -0,0 +1,16 @@
+from .box import Box
+from .button import Button
+from .centerbox import CenterBox
+from .chart import Chart
+from .circular_progress_bar import CircularProgressBar
+from .container import Container
+from .date_time import DateTime
+from .entry import Entry
+from .eventbox import EventBox
+from .image import Image
+from .label import Label
+from .overlay import Overlay
+from .revealer import Revealer
+from .stack import Stack
+from .wayland import Window as WaylandWindow
+from .x11 import Window as X11Window

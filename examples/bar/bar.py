9d20735c7c5119dc9aba15a0cd00c63a1311205b
BoolmanO <ponos585858@gmail.com>
2024-02-06 21:26:01 +0600

    NEW API IN EXAMPLES WITH RUFF FORMAT

--- examples/bar/bar.py
+++ examples/bar/bar.py
@@ -107,7 +107,9 @@ class StatusBar(Window):
                 test_title=lambda x, max_length=20: (
                     "Desktop"
                     if len(x) == 0
-                    else x if len(x) <= max_length else x[: max_length - 3] + "..."
+                    else x
+                    if len(x) <= max_length
+                    else x[: max_length - 3] + "..."
                 ),
             ),
             name="hyprland-window",

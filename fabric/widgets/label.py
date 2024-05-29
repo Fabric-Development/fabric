439578d11ec89858cd6f1f367fa072275ec7e0e2
Adriano Tisera <66109459+Axenide@users.noreply.github.com>
2024-05-29 12:49:01 -0300

    feat: add markup support for `Label`  (#27)

--- fabric/widgets/label.py
+++ fabric/widgets/label.py
@@ -10,6 +10,7 @@ class Label(Gtk.Label, Widget):
     def __init__(
         self,
         label: str | None = None,
+        markup: bool = False,
         justfication: Literal[
             "left",
             "right",
@@ -50,6 +51,8 @@ class Label(Gtk.Label, Widget):
         """
         :param label: the actual label text, defaults to None
         :type label: str | None, optional
+        :param markup: whether to use markup or plain text, defaults to False
+        :type markup: bool, optional
         :param justfication: justfication mode, defaults to None
         :type justfication: Literal["left", "right", "center", "fill",] | Gtk.Justification | None, optional
         :param ellipsization: ellipsization mode, defaults to None
@@ -106,7 +109,12 @@ class Label(Gtk.Label, Widget):
             name,
             size,
         )
-        self.set_label(label) if label is not None else None
+
+        if label is not None and markup is True:
+            self.set_markup(label)
+        elif label is not None:
+            self.set_label(label)
+
         self.set_justify(
             {
                 "left": Gtk.Justification.LEFT,

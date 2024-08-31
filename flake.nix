9647e9a288acf625880a933c53b835670e38e6ce
wholikeel <mail@michaellepera.xyz>
2024-08-31 20:00:56 +0930

    refactor: Remove nixpkgs-unstable input
    Now using stable version of `ruff`;
    Much cleaner flake

--- flake.nix
+++ flake.nix
@@ -1,72 +1,63 @@
 {
   description = ''
-    next-gen framework for building desktop widgets using Python (check the rewrite branch for progress)
+    next-gen framework for building desktop widgets using Python
+    (check the rewrite branch for progress)
   '';
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/24.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    nixpkgs-unstable,
-    utils,
-    ...
-  }:
-    utils.lib.eachDefaultSystem
-    (system: let
-      overlay = final: prev: {
-        pythonPackagesExtensions =
-          prev.pythonPackagesExtensions
-          ++ [
-            (
-              python-final: python-prev: {
-                fabric-widgets = prev.callPackage ./default.nix {};
-              }
-            )
-          ];
-      };
-
-      pkgs = nixpkgs.legacyPackages.${system}.extend overlay;
-      unstable-pkgs = nixpkgs-unstable.legacyPackages.${system};
-
-      pythonWithPackages = pkgs.python3.withPackages (ps:
-        with ps; [
-          setuptools
-          wheel
-          build
-          click
-          pycairo
-          pygobject3
-          loguru
-          psutil
-          fabric-widgets
-        ]);
-    in {
-      packages = {
-        default = pkgs.python3Packages.fabric-widgets;
-      };
-
-      overlays.default = overlay;
-
-      devShells = {
-        default = pkgs.mkShell {
-          name = "fabric-shell";
-          packages = [
-            unstable-pkgs.basedpyright
-            unstable-pkgs.ruff
-            pythonWithPackages
-            pkgs.gtk3
-            pkgs.gtk-layer-shell
-            pkgs.cairo
-            pkgs.gobject-introspection
-            pkgs.libdbusmenu-gtk3
-            pkgs.gdk-pixbuf
-            pkgs.gnome.gnome-bluetooth
+  outputs =
+    { nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlay = final: prev: {
+          pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+            (python-final: python-prev: {
+              # `fabric` was taken
+              fabric-widgets = prev.callPackage ./default.nix { };
+            })
           ];
         };
-      };
-    });
+
+        pkgs = nixpkgs.legacyPackages.${system}.extend overlay;
+      in
+      {
+        packages.default = pkgs.python3Packages.fabric-widgets;
+        overlays.default = overlay;
+        formatter = pkgs.nixfmt-rfc-style;
+
+        devShells = {
+          default = pkgs.mkShell {
+            name = "fabric-shell";
+            packages = with pkgs; [
+              ruff
+              gtk3
+              gtk-layer-shell
+              cairo
+              gobject-introspection
+              libdbusmenu-gtk3
+              gdk-pixbuf
+              gnome.gnome-bluetooth
+              (python3.withPackages (
+                ps: with ps; [
+                  setuptools
+                  wheel
+                  build
+                  click
+                  pycairo
+                  pygobject3
+                  loguru
+                  psutil
+                  fabric-widgets
+                ]
+              ))
+            ];
+          };
+        };
+      }
+    );
 }

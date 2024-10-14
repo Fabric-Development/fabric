94b79ea0c5ece548caf8d6fad550ea2d8870c4f5
The Darsh <129121284+its-darsh@users.noreply.github.com>
2024-10-14 15:21:01 +0300

    feat: add more style features to `Corner`s (e.g. backgrounds and borders)

--- fabric/widgets/shapes/corner.py
+++ fabric/widgets/shapes/corner.py
@@ -19,9 +19,18 @@ class CornerOrientation(Enum):
 
 
 class Corner(Gtk.DrawingArea, Widget):
-    @Property(CornerOrientation, "readable")
+    @Property(CornerOrientation, "read-write")
     def orientation(self) -> CornerOrientation:
         return self._orientation
+
+    @orientation.setter
+    def oriorientation(
+        self,
+        value: Literal["top-left", "top-right", "bottom-left", "bottom-right"]
+        | CornerOrientation,
+    ):
+        self._orientation = get_enum_member(CornerOrientation, value)
+        return self.queue_draw()
 
     def __init__(
         self,
@@ -63,39 +72,59 @@ class Corner(Gtk.DrawingArea, Widget):
         self._orientation = get_enum_member(CornerOrientation, orientation)
         self.connect("draw", self.on_draw)
 
-    def on_draw(self, widget: "Corner", cr: cairo.Context):
-        aloc: cairo.Rectangle = self.get_allocation()  # type: ignore
-        # ^ hear me out, Gtk.Allocation == Gdk.Rectangle == cairo.Rectangle
-        width, height = aloc.width, aloc.height
-
-        context: Gtk.StyleContext = self.get_style_context()
-        background_color: Gdk.RGBA = context.get_background_color(Gtk.StateFlags.NORMAL)
-
-        cr.save()
-
-        Gdk.cairo_set_source_rgba(cr, background_color)
-
+    def do_render_shape(self, cr: cairo.Context, width: float, height: float):
         # _this is fine_
         match self.orientation:
             case CornerOrientation.TOP_LEFT:
-                cr.move_to(width, 0)
-                cr.curve_to(0, 0, 0, height, 0, height)
-                cr.line_to(0, 0)
-            case CornerOrientation.TOP_RIGHT:
-                cr.move_to(0, 0)
-                cr.curve_to(width, 0, width, height, width, height)
-                cr.line_to(width, 0)
-            case CornerOrientation.BOTTOM_LEFT:
-                cr.move_to(width, height)
-                cr.curve_to(0, height, 0, 0, 0, 0)
-                cr.line_to(0, height)
-            case CornerOrientation.BOTTOM_RIGHT:
                 cr.move_to(0, height)
-                cr.curve_to(width, height, width, 0, width, 0)
+                cr.line_to(0, 0)
+                cr.line_to(width, 0)
+                cr.curve_to(0, 0, 0, height, 0, height)
+            case CornerOrientation.TOP_RIGHT:
+                cr.move_to(width, height)
+                cr.line_to(width, 0)
+                cr.line_to(0, 0)
+                cr.curve_to(width, 0, width, height, width, height)
+            case CornerOrientation.BOTTOM_LEFT:
+                cr.move_to(0, 0)
+                cr.line_to(0, height)
                 cr.line_to(width, height)
+                cr.curve_to(0, height, 0, 0, 0, 0)
+            case CornerOrientation.BOTTOM_RIGHT:
+                cr.move_to(width, 0)
+                cr.line_to(width, height)
+                cr.line_to(0, height)
+                cr.curve_to(width, height, width, 0, width, 0)
+        return
 
+    def on_draw(self, _, cr: cairo.Context):
+        aloc: cairo.Rectangle = self.get_allocation()  # type: ignore
+        # ^ hear me out, Gtk.Allocation == Gdk.Rectangle == cairo.Rectangle
+        width, height = aloc.width, aloc.height
+        context: Gtk.StyleContext = self.get_style_context()
+        border_color: Gdk.RGBA = context.get_border_color(Gtk.StateFlags.NORMAL)  # type: ignore
+        border_width = (
+            max(
+                (border := context.get_border(Gtk.StateFlags.NORMAL)).top,  # type: ignore
+                border.bottom,  # type: ignore
+                border.left,  # type: ignore
+                border.right,  # type: ignore
+            )
+            * 2  # the power of two because half the size of the border is out the bounding box
+        )
+
+        cr.save()
+
+        self.do_render_shape(cr, width, height)
         cr.close_path()
-        cr.fill()
+
+        cr.clip()
+        Gtk.render_background(context, cr, 0, 0, width, height)
+
+        Gdk.cairo_set_source_rgba(cr, border_color)  # type: ignore
+        cr.set_line_width(border_width)
+        self.do_render_shape(cr, width, height)
+        cr.stroke()
 
         cr.restore()
         return

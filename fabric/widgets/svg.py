83f2641982496c61b10d09e498a73551e54334fb
The Darsh <129121284+its-darsh@users.noreply.github.com>
2024-11-01 16:01:20 +0200

    feat: improve sturcture and refactor

--- fabric/widgets/svg.py
+++ fabric/widgets/svg.py
@@ -1,13 +1,13 @@
 import gi
 import cairo
 from loguru import logger
-from collections.abc import Iterable
 from typing import Literal, overload
+from collections.abc import Iterable
 from fabric.widgets.widget import Widget
 from fabric.utils import compile_css
 
-gi.require_version("Rsvg", "2.0")
 gi.require_version("Gtk", "3.0")
+gi.require_version("Rsvg", "2.0")
 from gi.repository import Rsvg, Gtk
 
 
@@ -96,31 +96,77 @@ class Svg(Gtk.DrawingArea, Widget):
             **kwargs,
         )
         if svg_string and svg_file:
-            raise ValueError("both svg_string and svg_file can be set at the same time")
+            raise ValueError(
+                "both svg_string and svg_file can't be set at the same time"
+            )
+
         if not svg_string and not svg_file:
-            raise ValueError("please provide a resource to load the svg from")
+            raise ValueError("you must provide a source to load the svg from")
 
-        self._handle = (
-            Rsvg.Handle().new_from_data(svg_string.encode())  # type: ignore
-            if svg_string is not None
-            else Rsvg.Handle().new_from_file(svg_file)
-        )
+        if svg_file:
+            self._handle = Rsvg.Handle.new_from_file(svg_file)
+        else:
+            self._handle = Rsvg.Handle.new_from_data(svg_string.encode())  # type: ignore
 
+        self._style_compiled: str | None = None
+        if style:
+            self.set_style(style)
+
+        svg_size = self.do_get_svg_size()
         if size is not None:
             self.set_size_request(
-                *self.do_resize_with_aspect(
+                *self.do_calculate_size(
                     *(size if isinstance(size, (tuple, list)) else (size, size)),
-                    (self._handle.props.width / self._handle.props.height),  # type: ignore
+                    (svg_size[0] / svg_size[1]),  # type: ignore
                 )
             )
         else:
-            self.set_size_request(self._handle.props.width, self._handle.props.height)
+            self.set_size_request(*svg_size)
 
-        self._style_compiled: str | None = None
+    def do_get_svg_size(self) -> tuple[int, int]:
+        return self._handle.props.width, self._handle.props.height  # type: ignore
 
-        self.set_style(style) if style is not None else None
+    def do_get_viewport_rectangle(self) -> Rsvg.Rectangle:
+        alloc = self.get_allocation()
+        rect = Rsvg.Rectangle()
+        rect.x, rect.y, rect.width, rect.height = 0, 0, alloc.width, alloc.height  # type: ignore
+        return rect
 
-        self.connect("draw", self.on_draw)
+    def do_calculate_size(
+        self, width: int, height: int, aspect_ratio: float
+    ) -> tuple[int, int]:
+        cur_aspect = width / height
+        new_width, new_height = width, height
+        if cur_aspect > aspect_ratio:
+            new_width = height * aspect_ratio
+        else:
+            new_height = width / aspect_ratio
+        return round(new_width), round(new_height)
+
+    def do_draw(self, cr: cairo.Context):
+        if not self._handle:
+            return
+        cr.save()
+
+        cr.set_antialias(cairo.Antialias.BEST)
+
+        if self._style_compiled is not None and self._handle.set_stylesheet(
+            self._style_compiled.encode()  # type: ignore
+        ):
+            logger.error("[Svg] failed to apply style, probably invalid style property")
+
+        self._handle.set_dpi((self.get_scale_factor() * 160))
+        self._handle.render_document(cr, self.do_get_viewport_rectangle())  # type: ignore
+
+        cr.restore()
+
+    def do_finalize_handle(self):
+        if self._handle:
+            self._handle.free()
+
+            del self._handle
+            self._handle = None
+        return
 
     # override
     def set_style(
@@ -130,59 +176,23 @@ class Svg(Gtk.DrawingArea, Widget):
         append: bool = False,  # TODO: implement
         add_brackets: bool = True,
     ) -> None:
-        self._style_compiled = (
-            compile_css(
+        if compiled:
+            self._style_compiled = compile_css(
                 f"* {{ {style} }}"
                 if "{" not in style or "}" not in style and add_brackets is True
                 else style
             )
-            if compiled is True
-            else style
-        )
-        self.queue_draw()
-        return
-
-    def on_draw(self, widget: Gtk.DrawingArea, ctx: cairo.Context):
-        ctx.save()
-        if self._style_compiled is not None:
-            x = self._handle.set_stylesheet(self._style_compiled.encode())  # type: ignore
-            print(x, self._style_compiled)
-            if x is not True:
-                logger.error(
-                    "[Svg] failed to apply style, probably invalid style property"
-                )
-            del x
-        self.do_render_svg_for_ctx(ctx, self._handle)
-        ctx.restore()
-
-    def do_scale_to_fit_for_ctx(
-        self, ctx: cairo.Context, handle: Rsvg.Handle
-    ) -> cairo.Context:
-        allocation = self.get_allocation()
-        try:
-            scale_x = allocation.width / handle.props.width  # type: ignore
-            scale_y = allocation.height / handle.props.height  # type: ignore
-        except ZeroDivisionError:
-            scale_x = 1
-            scale_y = 1
-
-        ctx.scale(scale_x, scale_y)
-        return ctx
-
-    def do_render_svg_for_ctx(self, ctx: cairo.Context, handle: Rsvg.Handle):
-        handle.set_dpi((self.get_scale_factor() * 160))
-        ctx.set_antialias(cairo.Antialias.BEST)
-        self.do_scale_to_fit_for_ctx(ctx, handle)
-        handle.render_cairo(ctx)
-        return ctx
-
-    def do_resize_with_aspect(
-        self, width: int, height: int, aspect_ratio: float
-    ) -> tuple[int, int]:
-        cur_aspect = width / height
-        new_width, new_height = width, height
-        if cur_aspect > aspect_ratio:
-            new_width = height * aspect_ratio
         else:
-            new_height = width / aspect_ratio
-        return int(new_width), int(new_height)
+            self._style_compiled = style
+
+        return self.queue_draw()
+
+    def set_from_file(self, file: str):
+        self.do_finalize_handle()
+        self._handle = Rsvg.Handle.new_from_file(file)
+        return self.queue_draw()
+
+    def set_from_string(self, string: str):
+        self.do_finalize_handle()
+        self._handle = Rsvg.Handle.new_from_data(string.encode())  # type: ignore
+        return self.queue_draw()

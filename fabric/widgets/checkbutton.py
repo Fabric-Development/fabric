3662e94711d189d25b73e1f0c8cf4cfff8b77902
senchpimy <107078096+senchpimy@users.noreply.github.com>
2024-12-16 11:48:43 -0600

    feat: fabricate `CheckButton` (#61)

---
+++ fabric/widgets/checkbutton.py
@@ -0,0 +1,51 @@
+import gi
+from typing import Literal
+from collections.abc import Iterable
+from fabric.widgets.button import Button
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class CheckButton(Gtk.CheckButton, Button):
+    def __init__(
+        self,
+        label: str | None = None,
+        name: str | None = None,
+        visible: bool = True,
+        all_visible: bool = False,
+        style: str | None = None,
+        style_classes: Iterable[str] | str | None = None,
+        tooltip_text: str | None = None,
+        tooltip_markup: str | None = None,
+        h_align: Literal["fill", "start", "end", "center", "baseline"]
+        | Gtk.Align
+        | None = None,
+        v_align: Literal["fill", "start", "end", "center", "baseline"]
+        | Gtk.Align
+        | None = None,
+        h_expand: bool = False,
+        v_expand: bool = False,
+        size: Iterable[int] | int | None = None,
+        **kwargs,
+    ):
+        Gtk.CheckButton.__init__(self)  # type: ignore
+        Button.__init__(
+            self,
+            label,
+            None,
+            None,
+            name,
+            visible,
+            all_visible,
+            style,
+            style_classes,
+            tooltip_text,
+            tooltip_markup,
+            h_align,
+            v_align,
+            h_expand,
+            v_expand,
+            size,
+            **kwargs,
+        )

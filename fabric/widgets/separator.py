1ae3370d8888c173c2f203e96d3974250c6f756c
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-06-01 17:40:09 +0545

    feat: add separator

---
+++ fabric/widgets/separator.py
@@ -0,0 +1,60 @@
+from collections.abc import Iterable
+from typing import Literal
+
+from fabric.utils.helpers import get_enum_member
+from fabric.widgets.widget import Widget
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class Separator(Gtk.Separator, Widget):
+
+    def __init__(
+        self,
+        name: str | None = None,
+        visible: bool = True,
+        all_visible: bool = False,
+        orientation: Literal[
+            "horizontal",
+            "vertical",
+            "h",
+            "v",
+        ]
+        | Gtk.Orientation = Gtk.Orientation.HORIZONTAL,
+        style: str | None = None,
+        style_classes: Iterable[str] | str | None = None,
+        tooltip_text: str | None = None,
+        tooltip_markup: str | None = None,
+        h_align: Literal["fill", "start", "end", "center", "baseline"]
+        | Gtk.Align
+        | None = None,
+        v_align: Literal["fill", "start", "end", "center", "baseline"]
+        | Gtk.Align
+        | None = None,
+        h_expand: bool = False,
+        v_expand: bool = False,
+        size: Iterable[int] | int | None = None,
+        **kwargs,
+    ):
+        Gtk.Separator.__init__(self)
+        Widget.__init__(
+            self,
+            name,
+            visible,
+            all_visible,
+            style,
+            style_classes,
+            tooltip_text,
+            tooltip_markup,
+            h_align,
+            v_align,
+            h_expand,
+            v_expand,
+            size,
+            **kwargs,
+        )
+        self.set_orientation(
+            get_enum_member(
+                Gtk.Orientation, orientation, default=Gtk.Orientation.HORIZONTAL
+            )
+        )

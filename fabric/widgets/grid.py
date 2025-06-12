b8b5d2b5d3dfcedcd7f1dcfa31230a736d566bd6
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-06-13 00:15:31 +0545

    feat: flow grid

--- fabric/widgets/grid.py
+++ fabric/widgets/grid.py
@@ -1,20 +1,21 @@
-from typing import Literal
 from collections.abc import Iterable
+from typing import Literal
+
 from fabric.widgets.widget import Widget
-gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 
 class Grid(Gtk.Grid, Widget):
+
     def __init__(
         self,
+        name: str | None = None,
+        visible: bool = True,
+        all_visible: bool = False,
         row_spacing: int = 0,
         column_spacing: int = 0,
         column_homogeneous: bool = False,
         row_homogeneous: bool = False,
-        name: str | None = None,
-        visible: bool = True,
-        all_visible: bool = False,
         style: str | None = None,
         style_classes: Iterable[str] | str | None = None,
         tooltip_text: str | None = None,
@@ -51,3 +52,20 @@ class Grid(Gtk.Grid, Widget):
         self.set_column_spacing(column_spacing)
         self.set_column_homogeneous(column_homogeneous)
         self.set_row_homogeneous(row_homogeneous)
+
+    def attach_flow(
+        self, children: Iterable[Widget], columns, start_row=0, start_col=0
+    ):
+        """
+        Adds widgets to a Gtk.Grid in a flow layout.
+
+        Args:
+            children (list): List of Gtk.Widget to add.
+            columns (int): Number of columns in the grid.
+            start_row (int): Optional starting row.
+            start_col (int): Optional starting column.
+        """
+        for index, child in enumerate(children):
+            row = start_row + index // columns
+            col = start_col + index % columns
+            self.attach(child, col, row, 1, 1)

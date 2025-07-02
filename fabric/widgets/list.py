4fa64dfe69712e0f5719dd009b9c28d2aca0e4d2
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-07-02 21:43:27 +0545

    feat: fabricate listbox

---
+++ fabric/widgets/list.py
@@ -0,0 +1,55 @@
+from collections.abc import Iterable
+from typing import Literal
+
+import gi
+from fabric.widgets.widget import Widget
+from gi.repository import Gtk
+
+gi.require_version("Gtk", "3.0")
+
+
+class ListBox(Gtk.ListBox, Widget):
+  
+    def __init__(
+        self,
+        name: str | None = None,
+        visible: bool = True,
+        all_visible: bool = False,
+        style: str | None = None,
+        style_classes: Iterable[str] | str | None = None,
+        tooltip_text: str | None = None,
+        tooltip_markup: str | None = None,
+        h_align: Literal["fill", "start", "end", "center", "baseline"]
+        | Gtk.Align
+        | None = None,
+        v_align: Literal["fill", "start", "end", "center", "baseline"]
+        | Gtk.Align
+        | None = None,
+        h_expand: bool = False,
+        v_expand: bool = False,
+        size: Iterable[int] | int | None = None,
+        **kwargs,
+    ):
+        Gtk.ListBox.__init__(self)
+        Widget.__init__(
+            self,
+            name,
+            visible,
+            all_visible,
+            style,
+            style_classes,
+            tooltip_text,
+            tooltip_markup,
+            h_align,
+            v_align,
+            h_expand,
+            v_expand,
+            size,
+            **kwargs,
+        )
+
+    def remove_all(self) -> None:
+        """Remove all children from the list box."""
+        for child in self.get_children():
+            self.remove(child)
+            child.destroy()

19c1dfba337d29099d9f62a53e3003a1893c6e34
Rishi Vora <vorarishi22+github@gmail.com>
2025-07-04 01:49:24 +0530

    feat(nix): reintroduce flake implementation (#121)

--- default.nix
+++ default.nix
@@ -1,21 +1,49 @@
 {
   lib,
-  python312Packages,
+  pkg-config,
+  wrapGAppsHook3,
+  gobject-introspection,
+  python3,
+  python3Packages,
+  runtimeShell,
   gtk3,
   gtk-layer-shell,
   cairo,
-  gobject-introspection,
   libdbusmenu-gtk3,
   gdk-pixbuf,
   cinnamon-desktop,
   gnome-bluetooth,
+  extraPackages ? [ ],
+  extraDependencies ? [ ],
 }:
-python312Packages.buildPythonPackage {
+let
+  pydeps =
+    ps:
+    with ps;
+    [
+      click
+      pycairo
+      pygobject3
+      pygobject-stubs
+      loguru
+      psutil
+    ]
+    ++ extraDependencies;
+in
+python3Packages.buildPythonPackage {
   pname = "python-fabric";
   version = "0.0.2";
   pyproject = true;
 
   src = ./.;
+
+  build-system = [ python3Packages.setuptools ];
+
+  nativeBuildInputs = [
+    pkg-config
+    gobject-introspection
+    wrapGAppsHook3
+  ];
 
   buildInputs = [
     gtk3
@@ -26,25 +54,31 @@ python312Packages.buildPythonPackage {
     gdk-pixbuf
     cinnamon-desktop
     gnome-bluetooth
-  ];
+  ] ++ extraPackages;
 
-  dependencies = with python312Packages; [
-    setuptools
-    click
-    pycairo
-    pygobject3
-    pygobject-stubs
-    loguru
-    psutil
-  ];
+  dependencies = pydeps python3Packages;
+
+  postInstall =
+    let
+      py = python3.withPackages pydeps;
+    in
+    ''
+      mkdir -p $out/bin
+      cat > $out/bin/fabric << EOF
+      #!${runtimeShell}
+      export PYTHONPATH="${toString ./.}:${py}/lib/python${py.python.version}/site-packages"
+      GI_TYPELIB_PATH=$GI_TYPELIB_PATH \
+      GDK_PIXBUF_MODULE_FILE="$GDK_PIXBUF_MODULE_FILE" \
+      ${py.interpreter} -m fabric "\$@"
+      EOF
+      chmod +x $out/bin/fabric
+    '';
 
   meta = {
-    changelog = "";
-    description = ''
-      next-gen framework for building desktop widgets using Python (check the rewrite branch for progress)
-    '';
+    changelog = "https://github.com/Fabric-Development/fabric/blob/master/CHANGELOG.md";
+    description = "The next-generation framework for building desktop widgets using Python";
     homepage = "https://github.com/Fabric-Development/fabric";
     license = lib.licenses.agpl3Plus;
-    maintainers = with lib.maintainers; [];
+    maintainers = with lib.maintainers; [ ];
   };
 }

10d11b00374ce8934e3a12fc90ba3f16b5cd2a58
The Darsh <129121284+its-darsh@users.noreply.github.com>
2025-07-06 01:05:22 +0300

    feat: revisit example bar

--- examples/bar/config.py
+++ examples/bar/config.py
@@ -1,8 +1,7 @@
 import psutil
-from fabric import Application
+from fabric import Application, Fabricator
 from fabric.widgets.box import Box
-from fabric.widgets.label import Label
-from fabric.widgets.overlay import Overlay
+from fabric.widgets.image import Image
 from fabric.widgets.eventbox import EventBox
 from fabric.widgets.datetime import DateTime
 from fabric.widgets.centerbox import CenterBox
@@ -10,12 +9,7 @@ from fabric.system_tray.widgets import SystemTray
 from fabric.widgets.circularprogressbar import CircularProgressBar
 from fabric.widgets.wayland import WaylandWindow as Window
 from fabric.hyprland.widgets import Language, ActiveWindow, Workspaces, WorkspaceButton
-from fabric.utils import (
-    FormattedString,
-    bulk_replace,
-    invoke_repeater,
-    get_relative_path,
-)
+from fabric.utils import FormattedString, get_relative_path, bulk_replace
 
 AUDIO_WIDGET = True
 
@@ -23,33 +17,27 @@ if AUDIO_WIDGET is True:
     try:
         from fabric.audio.service import Audio
     except Exception as e:
-        print(e)
         AUDIO_WIDGET = False
+        print(e)
 
 
 class VolumeWidget(Box):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.audio = Audio()
-
         self.progress_bar = CircularProgressBar(
-            name="volume-progress-bar", pie=True, size=24
+            name="volume-progress-bar",
+            pie=True,
+            child=Image(icon_name="audio-speakers-symbolic", icon_size=12),
+            size=24,
         )
 
-        self.event_box = EventBox(
-            events="scroll",
-            child=Overlay(
-                child=self.progress_bar,
-                overlays=Label(
-                    label="",
-                    style="margin: 0px 6px 0px 0px; font-size: 12px",  # to center the icon glyph
-                ),
+        self.audio = Audio(notify_speaker=self.on_speaker_changed)
+
+        super().__init__(
+            children=EventBox(
+                events="scroll", child=self.progress_bar, on_scroll_event=self.on_scroll
             ),
+            **kwargs,
         )
-
-        self.audio.connect("notify::speaker", self.on_speaker_changed)
-        self.event_box.connect("scroll-event", self.on_scroll)
-        self.add(self.event_box)
 
     def on_scroll(self, _, event):
         match event.direction:
@@ -59,14 +47,14 @@ class VolumeWidget(Box):
                 self.audio.speaker.volume -= 8
         return
 
-    def on_speaker_changed(self, *_):
+    def on_speaker_changed(self):
         if not self.audio.speaker:
             return
+
         self.progress_bar.value = self.audio.speaker.volume / 100
-        self.audio.speaker.bind(
+        return self.audio.speaker.bind(
             "volume", "value", self.progress_bar, lambda _, v: v / 100
         )
-        return
 
 
 class StatusBar(Window):
@@ -80,86 +68,81 @@ class StatusBar(Window):
             margin="10px 10px -2px 10px",
             exclusivity="auto",
             visible=False,
-            all_visible=False,
-        )
-        self.workspaces = Workspaces(
-            name="workspaces",
-            spacing=4,
-            buttons_factory=lambda ws_id: WorkspaceButton(id=ws_id, label=None),
-        )
-        self.active_window = ActiveWindow(name="hyprland-window")
-        self.language = Language(
-            formatter=FormattedString(
-                "{replace_lang(language)}",
-                replace_lang=lambda lang: bulk_replace(
-                    lang,
-                    (r".*Eng.*", r".*Ar.*"),
-                    ("ENG", "ARA"),
-                    regex=True,
-                ),
-            ),
-            name="hyprland-window",
-        )
-        self.date_time = DateTime(name="date-time")
-        self.system_tray = SystemTray(name="system-tray", spacing=4)
-
-        self.ram_progress_bar = CircularProgressBar(
-            name="ram-progress-bar", pie=True, size=24
-        )
-        self.cpu_progress_bar = CircularProgressBar(
-            name="cpu-progress-bar", pie=True, size=24
-        )
-        self.progress_bars_overlay = Overlay(
-            child=self.ram_progress_bar,
-            overlays=[
-                self.cpu_progress_bar,
-                Label("", style="margin: 0px 6px 0px 0px; font-size: 12px"),
-            ],
         )
 
-        self.status_container = Box(
-            name="widgets-container",
+        self.system_status = Box(
+            name="system-status",
             spacing=4,
             orientation="h",
-            children=self.progress_bars_overlay,
+            children=[
+                # a progress bar (ram) has a child of a progress bar (cpu) that which has a child (the icon)
+                CircularProgressBar(
+                    name="ram-progress-bar",
+                    pie=True,
+                    child=CircularProgressBar(
+                        name="cpu-progress-bar",
+                        pie=True,
+                        child=Image(icon_name="cpu-symbolic", icon_size=12),
+                        size=24,
+                    ).build(
+                        lambda progres: Fabricator(
+                            interval=1000,
+                            poll_from=lambda f: psutil.cpu_percent() / 100,
+                            on_changed=lambda _, value: progres.set_value(value),
+                        )
+                    ),
+                    size=24,
+                ).build(
+                    lambda progres: Fabricator(
+                        interval=1000,
+                        poll_from=lambda f: psutil.virtual_memory().percent / 100,
+                        on_changed=lambda _, value: progres.set_value(value),
+                    )
+                )
+            ]
+            # create a volume widget if enabled
+            + ([VolumeWidget()] if AUDIO_WIDGET else []),
         )
-        self.status_container.add(VolumeWidget()) if AUDIO_WIDGET is True else None
 
         self.children = CenterBox(
             name="bar-inner",
             start_children=Box(
                 name="start-container",
-                spacing=4,
-                orientation="h",
-                children=self.workspaces,
+                children=Workspaces(
+                    name="workspaces",
+                    spacing=4,
+                    buttons_factory=lambda ws_id: WorkspaceButton(id=ws_id, label=None),
+                ),
             ),
             center_children=Box(
                 name="center-container",
-                spacing=4,
-                orientation="h",
-                children=self.active_window,
+                children=ActiveWindow(name="hyprland-window"),
             ),
             end_children=Box(
                 name="end-container",
                 spacing=4,
                 orientation="h",
                 children=[
-                    self.status_container,
-                    self.system_tray,
-                    self.date_time,
-                    self.language,
+                    self.system_status,
+                    SystemTray(name="system-tray", spacing=4),
+                    DateTime(name="date-time"),
+                    Language(
+                        name="hyprland-window",
+                        formatter=FormattedString(
+                            "{replace_lang(language)}",
+                            replace_lang=lambda lang: bulk_replace(
+                                lang,
+                                (r".*Eng.*", r".*Ar.*"),
+                                ("ENG", "ARA"),
+                                regex=True,
+                            ),
+                        ),
+                    ),
                 ],
             ),
         )
 
-        invoke_repeater(1000, self.update_progress_bars)
-
-        self.show_all()
-
-    def update_progress_bars(self):
-        self.ram_progress_bar.value = psutil.virtual_memory().percent / 100
-        self.cpu_progress_bar.value = psutil.cpu_percent() / 100
-        return True
+        return self.show_all()
 
 
 if __name__ == "__main__":

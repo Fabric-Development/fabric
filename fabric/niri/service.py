670292f321789c79abe1ef9f5a3389c7e71f4f9b
packetverse <60551416+packetverse@users.noreply.github.com>
2025-07-20 00:57:57 +0200

    fix: refactoring and proper lookup_socket function

--- fabric/niri/service.py
+++ fabric/niri/service.py
@@ -1,8 +1,9 @@
 import os
 import json
-from dataclasses import dataclass
-from typing import Callable, Any, ParamSpec, Concatenate
 
+from dataclasses import dataclass
+from typing import ParamSpec, Concatenate, Any
+from collections.abc import Callable
 from loguru import logger
 from gi.repository import Gio, GLib
 
@@ -12,11 +13,9 @@ from fabric.utils.helpers import idle_add
 P = ParamSpec("P")
 NIRI_COMMAND_BUFFER_SIZE = 1_048_576  # 1MB
 
-# Exceptions
 class NiriError(Exception): ...
 class NiriSocketNotFoundError(Exception): ...
 
-# Dataclasses
 @dataclass(frozen=True)
 class NiriReply:
     command: str
@@ -29,11 +28,12 @@ class NiriEvent:
     data: dict
     raw: str
 
-# Niri Service
 class Niri(Service):
     """
     A Fabric-compatible service to interact with the Niri window manager via its IPC socket.
     """
+
+    SOCKET_PATH = ""
 
     @Property(bool, "readable", "is-ready", default_value=False)
     def ready(self) -> bool:
@@ -49,10 +49,10 @@ class Niri(Service):
     def __init__(self, commands_only: bool = False, **kwargs):
         super().__init__(**kwargs)
         self._ready = False
+        self.lookup_socket()
 
-        self.socket_path = os.getenv("NIRI_SOCKET")
-        if not self.socket_path or not os.path.exists(self.socket_path):
-            raise NiriSocketNotFoundError("NIRI_SOCKET not found or invalid.")
+        # if not self.socket_path or not os.path.exists(self.socket_path):
+        #     raise NiriSocketNotFoundError("NIRI_SOCKET not found or invalid.")
 
         if not commands_only:
             self.event_socket_thread = GLib.Thread.new(
@@ -63,30 +63,38 @@ class Niri(Service):
         self.ready.emit()
 
     @staticmethod
+    def lookup_socket() -> str:
+        if not os.path.exists(os.getenv("NIRI_SOCKET")):
+            raise NiriSocketNotFoundError("NIRI_SOCKET not found or invalid.")
+
+    @staticmethod
     def send_command(command: str | dict) -> NiriReply:
         """
         Send a command (e.g. "Workspaces") to the Niri IPC socket.
         """
         response = {}
         try:
+            address = Niri.lookup_socket()
+
             client = Gio.SocketClient()
-            address = Gio.UnixSocketAddress.new(os.getenv("NIRI_SOCKET"))
             conn: Gio.SocketConnection = client.connect(address)
-            ostream = conn.get_output_stream()
-            istream = Gio.DataInputStream.new(conn.get_input_stream())
+            stream: Gio.OutputStream = conn.get_output_stream()
+
+            # ostream = conn.get_output_stream()
+            # istream = Gio.DataInputStream.new(conn.get_input_stream())
 
             if isinstance(command, dict):
-                ostream.write((json.dumps(command) + "\n").encode())
+                stream.write((json.dumps(command) + "\n").encode())
             else:
-                ostream.write((f'"{command}"\n').encode())
-                # ostream.write((f'"{command}"\n'))
-            ostream.flush()
+                stream.write((f'"{command}"\n').encode())
 
-            raw_data = istream.read_line_utf8(None)[0]
+            stream.flush()
+
+            input_stream = Gio.DataInputStream.new(conn.get_input_stream())
+            raw_data = input_stream.read_line_utf8(None)[0]
             response = json.loads(raw_data)
         except Exception as e:
             logger.error(f"[NiriService] Failed to send command '{command}': {e}")
-            return NiriReply(command=command, reply={}, is_ok=False)
 
         return NiriReply(command=command, reply=response, is_ok="Ok" in response)
 
@@ -97,46 +105,45 @@ class Niri(Service):
         *args: P.args,
         **kwargs: P.kwargs,
     ) -> None:
+        address = Niri.lookup_socket()
         client = Gio.SocketClient()
-        address = Gio.UnixSocketAddress.new(os.getenv("NIRI_SOCKET"))
 
-        def reader_callback(istream: Gio.DataInputStream, res: Gio.AsyncResult, *_):
+        def reader_callback(input_stream: Gio.DataInputStream, res: Gio.AsyncResult, *_):
             try:
-                # raw = istream.read_line_finish_utf8(res)[0].decode()
-                raw = istream.read_line_finish_utf8(res)[0]
-                data = json.loads(raw)
-                reply = NiriReply(command=command, reply=data, is_ok="Ok" in data)
+                raw_data = input_stream.read_line_finish_utf8(res)[0]
+                reply = NiriReply(command=command, reply=json.loads(raw), is_ok="Ok" in data)
             except Exception as e:
                 logger.error(f"[NiriService] Async command error: {e}")
                 reply = NiriReply(command=command, reply={}, is_ok=False)
 
             callback(reply, *args, **kwargs)
 
-        def connect_callback(client: Gio.SocketClient, res: Gio.AsyncResult, *_):
+        def client_callback(client: Gio.SocketClient, res: Gio.AsyncResult, *_):
             try:
                 conn: Gio.SocketConnection = client.connect_finish(res)
-                ostream = conn.get_output_stream()
-                istream = Gio.DataInputStream.new(conn.get_input_stream())
+                stream: Gio.OutputStream = conn.get_output_stream()
+                input_stream = Gio.DataInputStream.new(conn.get_input_stream())
 
-                ostream.write_async(
+                stream.write_async(
                     f'"{command}"\n'.encode(),
                     NIRI_COMMAND_BUFFER_SIZE,
                     None, None, None, None
                 )
-                ostream.flush_async(1, None, None, None)
-                istream.read_line_async(1, None, reader_callback, conn)
+                stream.flush_async(1, None, None, None)
+                input_stream.read_line_async(1, None, reader_callback, conn)
             except Exception as e:
                 logger.error(f"[NiriService] Async connect error: {e}")
 
-        client.connect_async(address, None, connect_callback, None)
+        client.connect_async(address, None, client_callback, None)
+        return None
 
     def event_socket_task(self, _) -> bool:
         """
         Listens for events using Niri's 'EventStream' mechanism.
         """
         try:
+            address = Niri.lookup_socket()
             client = Gio.SocketClient()
-            address = Gio.UnixSocketAddress.new(self.socket_path)
             conn: Gio.SocketConnection = client.connect(address)
             ostream = conn.get_output_stream()
             istream = Gio.DataInputStream.new(conn.get_input_stream())
@@ -156,10 +163,6 @@ class Niri(Service):
                     event_name = next(iter(event_data))
                     event_obj = NiriEvent(name=event_name, data=event_data[event_name], raw=raw_str)
 
-                    # logger.debug(f"Emitting event: {event_name} with data: {event_data}")
-                    # logger.debug(f"Event Object: {event_obj}")
-
-                    # idle_add(self.emit, f"event::{event_name}", event_obj)
                     idle_add(self.emit, f"event::{event_name}", event_obj)
                 except Exception as e:
                     logger.error(f"[NiriService] Event parse error: {e}")
